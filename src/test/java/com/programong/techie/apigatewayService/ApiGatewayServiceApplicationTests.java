c9e4ced1e9661c0333d4253534fbd6cd1d44f073
unknown <ravindranagode11@gmail.com>
2023-10-15 19:31:00 +0530

    This is gatway api project

---
+++ src/test/java/com/programong/techie/apigatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programong.techie.apigatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
